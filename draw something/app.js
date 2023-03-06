444e79f4fb1803257a35c2dcec684bafc3169833
Will Burdge <123422158+WillBurdge@users.noreply.github.com>
2023-03-06 00:50:25 -0800

    Add files via upload

---
+++ draw something/app.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+});
+
+ctx.strokeStyle = 'red';
+
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener("mouseout", () => {
+  isDrawing = false;
+});
+
